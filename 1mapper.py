07156b424e94da627dc8530054bc1aabdf11cd6d
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2020-02-10 13:36:43 -0600

    added spacing

--- 1mapper.py
+++ 1mapper.py
@@ -3,7 +3,7 @@ output = open("01.txt", "w")
 
 for line in input:
     datalist = line.strip().split("    ")
-    date, time,store, item, cost, paymentType = datalist
+    date, time, store, item, cost, paymentType = datalist
     output.write(store + "\t" + cost + "\n")
 
 input.close()
