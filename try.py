8852a385c6e734f763b287e1da92a491d8783cff
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2020-02-10 13:37:01 -0600

    tried different python functions

--- try.py
+++ try.py
@@ -1,4 +1,12 @@
-print ("hello, world!")
-print ('hello, bearcat!')
-print ('\n')
-
+f = open("d.txt","r")  # open file, read-only
+o = open("o.txt", "w") # open file, write
+for line in f:
+    data = line.strip().split("    ")
+    print (data)
+    #print (len(data))
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data
+        print ("{0}\t{1}".format(store, cost))
+        o.write("{0}\t{1}\n".format(store, cost))
+f.close()
+o.close()
